f76d192d0c959df66820007b18280a09d0a93a99
chenshenhai <cshenhai@gmail.com>
2017-05-31 00:00:22 +0800

    feat: add hot require lib

---
+++ index.js
@@ -0,0 +1,6 @@
+require('./lib/require');
+
+let Mod1 = $require('./modules/mod1');
+let Mod2 = $require('./modules/mod2');
+
+console.log(Mod1, Mod2)
---
+++ modules/mod1.js
@@ -0,0 +1,3 @@
+module.exports = function() {
+  this.data = 'module 001'
+}
