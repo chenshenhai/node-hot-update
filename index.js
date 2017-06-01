7dd6eff2cef2bc7a0bf447ff7b9f37108172a68e
chenshenhai <cshenhai@gmail.com>
2017-06-02 07:40:14 +0800

    feat: rewrite module.export

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-require('./lib/require');
+const hotRequire = require('./lib/require');
 
 setInterval(()=>{
-  let Mod1 = $require('./modules/mod1');
-  let Mod2 = $require('./modules/mod2');
+  // let Mod1 = $require('./modules/mod1');
+  // let Mod2 = $require('./modules/mod2');
+  let Mod1 = hotRequire('./modules/mod1');
+  let Mod2 = hotRequire('./modules/mod2');
   let m1 = new Mod1();
   let m2 = new Mod2();
 
