f807e6b05b37ebe8addcc5ce4486b821afde4733
chenshenhai <cshenhai@gmail.com>
2017-06-03 20:49:58 +0800

    feat: add update object.define props

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ setInterval(()=>{
   // let Mod2 = $require('./modules/mod2');
   let Mod1 = hotRequire('./modules/mod1');
   let Mod2 = hotRequire('./modules/mod2');
+  let Obj1 = hotRequire('./modules/obj1');
+
   let m1 = new Mod1();
   let m2 = new Mod2();
 
-  console.log(m1.data, m2.data)
+  console.log(m1.data, m2.data, Obj1.data);
 }, 3000)
